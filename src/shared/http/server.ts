493db0c3da5e3e7c1b4c01e8c26e54ff4cf7f5e4
Geraldo-git <laboratorio.inicial@gmail.com>
2023-02-23 18:29:03 -0300

    Estrutura de pastas e cors

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,1 +1,17 @@
-console.log('Olá Dev');
+import express from 'express';
+import cors from 'cors';
+import routes from './routes';
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+
+app.listen(3333, () => {
+    console.log('Servidor ativo pota 3333!');
+})
+
+
+
+
